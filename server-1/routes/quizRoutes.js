422797090ef8cc90d77b19838f0015e7d2a41b75
Senil <dimalkawijesundara@gmail.com>
2025-09-15 00:42:20 +0530

    update quiz route

--- server-1/routes/quizRoutes.js
+++ server-1/routes/quizRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middlewares/authMiddleware');
-const { getQuizQuestions, submitQuizResult, getUserResults, validateAnswers, evaluateQuiz,
+const { getQuizQuestions, submitQuizResult, getUserResults, evaluateQuiz,updateSkill,decodeToken,
         getF2QuizQuestions, evaluateQuizF2
     } = require('../controllers/quizController');
 
 //Function-1(Problem Solving)
 router.get('/quiz', protect, getQuizQuestions);
-router.post('/quiz/evaluate', protect, evaluateQuiz);
+router.post("/update-skill", protect, updateSkill);
+
+router.post('/evaluate', protect, evaluateQuiz);
+router.get("/decode", decodeToken);
+
 
 router.post('/submit', protect, submitQuizResult);
-router.get('/my-', protect, getUserResults);
-
-router.post('/validate-answers', protect, validateAnswers);
+router.get("/results", protect, getUserResults);
+// router.post('/validate-answers', protect, validateAnswers);
 
 //Function-2()
 router.get('/quiz-f2', protect, getF2QuizQuestions);
